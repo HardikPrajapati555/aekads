2b9b57e575c3c1f560601a80ee7735270869be6a
Hardik Prajapati <135410619+HardikPrajapati555@users.noreply.github.com>
2024-07-03 16:37:20 +0530

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -4,7 +4,7 @@ const { Pool } = require("pg");
 
 const isProduction = process.env.NODE_ENV === "production";
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const connectionString = `postgres://avnadmin:AVNS_swf4UOvWo9x8sPVPIrg@pg-f7a2a44-hardikpatan2324-a09c.l.aivencloud.com:27885/defaultdb?sslmode=require`;
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
